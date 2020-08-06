b6e45869f00255ddd57d9d9d597bd249884d325b
Juliana Gutierrez <juliana.oguther@gmail.com>
2020-08-06 01:30:13 -0300

    commit maroto

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -29,17 +29,19 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL_TOP = window.location.hostname.includes('localhost')
-      ? 'http://localhost:8080/categorias'
-      : 'https://devjulianartflix.herokuapp.com/categorias';
-    fetch(URL_TOP)
-     .then(async (respostaDoServidor) => {
-        const resposta = await respostaDoServidor.json();
-       setCategorias([
-         ...resposta,
-       ]);
-      });
-
+    if(window.location.href.includes('localhost')) {
+      const URL = 'http://localhost:8080/categorias';
+      fetch(URL)
+       .then(async (respostaDoServer) =>{
+        if(respostaDoServer.ok) { 
+          const resposta = await respostaDoServer.json();
+       setCategorias(resposta);
+       return;
+     }
+     throw new Error('Não foi possível pegar os dados');
+    })
+  }
+   
    // setTimeout(() => {
    //   setCategorias([
    //    ...categorias,
